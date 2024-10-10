b17533f675be326534ff7c3d2012a272d6d29521
Lucca Zutin <luccazutin@gmail.com>
2024-10-10 15:59:25 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      
+      </div>
+      <h1>Vite + React</h1>
+      <div className='h-20 w-20 border'></div>
+      <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+
+    </>
+  )
+}
+
+export default App
