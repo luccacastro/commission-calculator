dd5be94dbc08e010cd5a7f96bc30486e6f167e93
Lucca Zutin <luccazutin@gmail.com>
2024-10-13 16:48:47 +0100

    fixed typing error

--- src/components/CommissionResult.tsx
+++ src/components/CommissionResult.tsx
@@ -4,7 +4,7 @@ import CountUp from 'react-countup';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 
-export const CommissionResult: React.FC<{ commission: number, revenue: number, loading: boolean }> = ({ commission, revenue, loading }) => (
+export const CommissionResult: React.FC<{ commission: number, revenue:  number | string, loading: boolean }> = ({ commission, revenue, loading }) => (
     <h3 className="text-5xl font-bold text-blue-600 text-center my-10">
       <p className="text-lg text-center text-gray-500 mb-4">Your commission based on the assessed value of 
         <span className='bold text-black mx-1'>£{revenue ? revenue.toLocaleString('en-GB') : "0"}</span> is:</p>
